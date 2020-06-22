c673621236c5f96c7b897646ecec16605f4f232a
SandeepaDevin <48104360+SandeepaDevin@users.noreply.github.com>
2020-06-22 20:27:36 +0530

    update instruction.js with platform guidelines

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,54 +1,60 @@
-import React, { Fragment, Component } from "react";
-import Navbar, { Footer } from "./Navbar";
+import React, { Fragment, Component } from 'react';
+import '../styles/instruction.scss';
+import { Navbar, Footer } from "./Navbar";
 import { Row, Container, Col } from "react-bootstrap";
 
-class Instruction extends Component {
-  state = {
-    content: "",
-    content2: "",
-  };
+class Instruction extends Component{
+    state = {
+        content: ''
+    }
 
-  Unilevel = () => {
-    this.setState({ content: "University Level instructions" });
-    this.setState({ content2: " mini-header" });
-    this.setState({ content3: "All the content under mini header" });
-    this.setState({ content4: "This is the second part" });
-  };
+    platlevel = () => {
+        this.setState({ content: 
+        <div>
+            <h4>Platform Guidelines</h4>
+            <p>1. Navigate to the login from the navigation bar</p>
+            <p>2. Enter the given @umora mail address with the relevant password to enter the competition.</p>
+            <p>3. On the bottom, there are prev and next buttons to switch between concurrent questions. The flag button can be used to flag any important   question the user may intend to refer later.</p>
+            <p>4. On top of the page, a question dashboard will be there to switch between any question as preferred. The colors of the buttons are indicated as follows.</p>
+            <p style={{paddingLeft:'4%'}}> &#9679; <b>Flagged questions - Red</b></p>
+            <p style={{paddingLeft:'4%'}}> &#9679; <b>Unanswered questions - White</b></p>
+            <p style={{paddingLeft:'4%'}}> &#9679; <b>Answered questions - Blue</b></p>
+            <p>5. The category button color may differ according to the complexity of the question.</p>
+            <p>6. Choose the answer button and progress as intended. Answers can be changed anytime before submission. ‘Remove answer’ button will make the question unanswered.</p>
+        </div> 
+        });
 
-  Scllevel = () => {
-    this.setState({ content: "here are the school level instruction" });
-    this.setState({ content2: " " });
-    this.setState({ content3: " " });
-    this.setState({ content4: " " });
-  };
-  componentDidMount() {
-    this.Scllevel();
-  }
-  render() {
-    return (
-      <Fragment>
-        <Navbar></Navbar>
-        <Container fluid className="instructions min80">
-          <Row>
-            <Col className="all-center">
-              <button onClick={this.Scllevel}>Platform Guidelines</button>
-            </Col>
-            <Col className="all-center">
-              <button onClick={this.Unilevel}>Competition Rules</button>
-            </Col>
-          </Row>
-          <div id="left">
-            <h3> {this.state.content}</h3>
-            <h2>{this.state.content2}</h2>
-            <h3>{this.state.content3}</h3>
-            <h2>{this.state.content4}</h2>
-          </div>
-        </Container>
-
-        <Footer></Footer>
-      </Fragment>
-    );
-  }
+    };
+    instruct=()=>{
+        this.setState({ content:
+        <div>
+            <h4>Competition Rules</h4>
+        </div>
+        });
+    }
+    render() {
+        return (
+            <Fragment>
+                <Navbar></Navbar>
+                <br/>
+                <Container>
+                <div className='instructions' > 
+                <section>       
+                        <Row>
+                            <Col><button className='button' onClick={this.platlevel}> Platform Guidelines</button></Col>
+                            <Col><button className='button' onClick={this.instruct}> Competition Rules</button></Col>
+                        </Row>
+                        <br/>    
+                        {this.state.content}  
+                </section>
+                </div>
+                </Container>
+                <Footer></Footer>          
+            </Fragment>    
+            
+        );
+    }
+    
 }
 
 export default Instruction;
